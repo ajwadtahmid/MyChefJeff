77770e47ad8c0dc4d257f65cd97a1ae1d5a53cde
Ajwad <dev@ajwadtahmid.com>
2024-06-27 22:35:29 -0700

    Basic flask server and react frontend

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MyChefJeff
+
+Introducing MyChefJeff, designed specifically for students to make cooking fun, easy, and educational. Whether you are a complete novice or a budding chef, MyChefJeff has everything you need to support you on your cooking journey.
+
+Features:
+1. Recipi Finder
+2. Inventory-Based Suggestions
+3. Ask Chef Jeff
+4. Nutritional Value Calculator
---
+++ backend/app.py
@@ -0,0 +1,39 @@
+# source venv/bin/activate
+
+import pathlib
+import textwrap
+from flask import Flask, jsonify
+from flask_cors import CORS
+import os
+import google.generativeai as genai
+from IPython.display import display, Markdown
+
+genai.configure(api_key='AIzaSyAew8iKWojPlMqXS_vx41V0PSPbddUdrSI')
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+@app.route('/api/message', methods=['GET'])
+def get_message():
+    return jsonify({'message': 'Hello from Flask!'})
+
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+chat = model.start_chat(history=[])
+
+def ai_chat(message):
+    response = chat.send_message(message)
+    return response
+
+# resp = ai_chat("Create a meal prep plan for a college student")
+# print(resp.text)
+# resp = ai_chat("Make something with high protein and without nuts")
+# print(resp.text)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
