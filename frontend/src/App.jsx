7b80748cc8711815dd999e8ee717e5a97782c1d1
davidjoves <davidjoves20@gmail.com>
2024-06-29 14:23:10 -0700

    added to hero
    -m

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,20 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 export default function App() {
     return (
         <>
-            <div className="bg-customGray font-rounded text-white min-h-screen p-4">
+            <div className="bg-customGray pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
                 <Header/>
+                <Hero />
 
-                
-                <div className="p-4 rounded">
+                {/* <div className="p-4 rounded">
                     <h1 className="text-h1 text-gradient">Testing With Chef Jeff</h1>
                     <h4 className="text-h4">Secondary Color</h4>
                 </div>
                 <h2 className="text-h2 text-primary-500 mt-4 text-gradient">Header with Primary Color</h2>
                 <button className="bg-accent-500 text-white p-2 rounded mt-4">Button with Accent Color</button>
-                <p className="text-text-500 mt-4">This is a paragraph with the text color.</p>
+                <p className="text-text-500 mt-4">This is a paragraph with the text color.</p> */}
             </div>
         </>
     );
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,17 @@
-import brand from '../assets/brand.svg'
+import brand from '../assets/brand.svg';
 
 const Header = () => {
     return (
         <div
-            className="bg-customGray fixed top-0 z-50 lg:bg-n-8/90
-            lg:backdrop-blue-sm w-full">
+            className="bg-customGray fixed top-0 w-full md:z-50 md:w-auto lg:bg-n-8/90 lg:backdrop-blue-sm sm:">
             <div
-                className="flex items-center justify-between lg:justify-start px-5 lg:px-7.5 xl:px-10 py-4"></div>
-            <a className='block w-[20rem] xl:mr-8 p-4'>
-                <img src={brand} className="floating-3d" alt=""/>
-            </a>
+                className="flex items-center justify-between lg:justify-start px-4 sm:px-5 lg:px-7.5 xl:px-10 py-2 sm:py-4">
+                <a className="block w-40 sm:w-[20rem] xl:mr-8 p-2 sm:p-4 overflow-visible">
+                    <img src={brand} className="floating-3d" alt="Brand logo"/>
+                </a>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
