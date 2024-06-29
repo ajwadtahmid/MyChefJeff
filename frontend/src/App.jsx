56d29dfe1759fe6f03171314d79b0af142b63595
davidjoves <davidjoves20@gmail.com>
2024-06-28 21:13:57 -0700

    -m testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,13 @@ export default function App() {
                 <Header/>
 
                 
-                {/* <div className="p-4 rounded">
+                <div className="p-4 rounded">
                     <h1 className="text-h1 text-gradient">Testing With Chef Jeff</h1>
                     <h4 className="text-h4">Secondary Color</h4>
                 </div>
                 <h2 className="text-h2 text-primary-500 mt-4 text-gradient">Header with Primary Color</h2>
                 <button className="bg-accent-500 text-white p-2 rounded mt-4">Button with Accent Color</button>
-                <p className="text-text-500 mt-4">This is a paragraph with the text color.</p> */}
+                <p className="text-text-500 mt-4">This is a paragraph with the text color.</p>
             </div>
         </>
     );
